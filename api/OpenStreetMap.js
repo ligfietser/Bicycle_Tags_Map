fe75895e0334ad7d6fa2b545ed46d52bf63725d7
Caleb Troyer <github@troyer.dev>
2024-01-03 23:09:40 -0500

    Update URLs, support cycleway:both tagging
    * Fix insecure/mixed content warning when not using proxy
    * Use preferred OpenStreetMap tile server (https://github.com/openstreetmap/operations/issues/737)
    * cycleway:both for shared_lane
    * cycleway:both for lane, shoulder, etc

--- api/OpenStreetMap.js
+++ api/OpenStreetMap.js
@@ -14,9 +14,7 @@ OpenLayers.Layer.OSM.Mapnik = OpenLayers.Class(OpenLayers.Layer.OSM, {
      */
     initialize: function(name, options) {
         var url = [
-            "http://a.tile.openstreetmap.org/${z}/${x}/${y}.png",
-            "http://b.tile.openstreetmap.org/${z}/${x}/${y}.png",
-            "http://c.tile.openstreetmap.org/${z}/${x}/${y}.png"
+            "http://tile.openstreetmap.org/${z}/${x}/${y}.png"
         ];
         options = OpenLayers.Util.extend({
             numZoomLevels: 20,
